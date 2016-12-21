d6bc25f19ce6003d765e9d630f016308594e0d8b
Pablo Roizo (External) <pablo.roizo@odigeo.org>
2016-12-21 17:53:53 +0100

    testing

--- dll/src/ReflectiveDll.c
+++ dll/src/ReflectiveDll.c
@@ -2,6 +2,7 @@
 // This is a stub for the actuall functionality of the DLL.
 //===============================================================================================//
 #include "ReflectiveLoader.h"
+#include <stdio.h>
 
 // Note: REFLECTIVEDLLINJECTION_VIA_LOADREMOTELIBRARYR and REFLECTIVEDLLINJECTION_CUSTOM_DLLMAIN are
 // defined in the project properties (Properties->C++->Preprocessor) so as we can specify our own 
@@ -10,6 +11,13 @@
 // You can use this value as a pseudo hinstDLL value (defined and set via ReflectiveLoader.c)
 extern HINSTANCE hAppInstance;
 //===============================================================================================//
+
+int getEBP(void) {
+	_asm {
+		mov eax, [ebp]
+	}
+}
+
 BOOL WINAPI DllMain( HINSTANCE hinstDLL, DWORD dwReason, LPVOID lpReserved )
 {
     BOOL bReturnValue = TRUE;
@@ -21,6 +29,27 @@ BOOL WINAPI DllMain( HINSTANCE hinstDLL, DWORD dwReason, LPVOID lpReserved )
 			break;
 		case DLL_PROCESS_ATTACH:
 			hAppInstance = hinstDLL;
+
+			HMODULE base = GetModuleHandle("dummy.exe");
+
+			printf("\n-------------------------\n");
+			printf("getEBP is %#x\n", (int)getEBP());
+			printf("Base is %#x\n\n", (int)base);
+
+			int * p = (int *)((int)base + 71983);
+			*p = 0x83;
+			p++;
+			*p = 0xE8;
+			p++;
+			*p = 0xFF;
+			
+			/* int * p = (int *)(0x00AFF928);
+			if (p != NULL) {
+				printf("\nVal in %p is %d\n\n", p, *p);
+				*p = 4000;
+				printf("\nNew val in %p is %d\n\n", p, *p);
+			}*/
+
 			MessageBoxA( NULL, "Hello from DllMain!", "Reflective Dll Injection", MB_OK );
 			break;
 		case DLL_PROCESS_DETACH:
